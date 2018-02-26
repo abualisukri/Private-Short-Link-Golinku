68dde607e133f75d765d9177f16d60b1bdd72fc0
abualisukri <31865559+abualisukri@users.noreply.github.com>
2018-02-26 14:37:59 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -309,7 +309,7 @@ $short_url = $bitly_response->data->url;
 	  <p><?php echo $year; ?></p> 
 	  <p>MalaySub (Stream) <span></span></p>
       <p><a href="<?php echo $short_url; ?>"><span><?php echo $short_url; ?></span></a></p> 
-	  <p>Telegram Channel: t.me/layanmoviesubmalay <span></span></p>
+	  <p>Channel: t.me/layanmoviesubmalay <span></span></p>
 	   <br><br>
          <span class="label label-warning">create by alisukri</span> 
 
